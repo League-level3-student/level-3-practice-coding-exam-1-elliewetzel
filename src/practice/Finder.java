2b6fa172f3cf93c90844c562d34ef4bc07b2930d
Ellie Wetzel <elliehw2005@gmail.com>
2020-04-06 19:53:02 -0700

    Level3PracticeB, Finder

--- src/practice/Finder.java
+++ src/practice/Finder.java
@@ -7,10 +7,13 @@ public class Finder {
 	public Object lastIndexOf(ArrayList<String> arrayList, String string) {
 		// TODO Auto-generated method stub
 		
-		
-		
-		
-		return null;
+		int lastIndexof = -1;
+		for(int i = 0; i < arrayList.size(); i++) {
+			if(arrayList.get(i).toLowerCase().equals(string.toLowerCase())) {
+				lastIndexof = i;
+			}
+		}
+		return lastIndexof;
 	}
 
 }
