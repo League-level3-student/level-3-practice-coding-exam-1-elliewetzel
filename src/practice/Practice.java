c3ad05ba9c705b26ab3ba2a0abb35c13021768db
Ellie Wetzel <elliehw2005@gmail.com>
2020-04-10 16:56:47 -0700

    practice

---
+++ src/practice/Practice.java
@@ -0,0 +1,48 @@
+package practice;
+import java.awt.Robot;
+
+public class Practice{
+	/*public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 4, 5, 6};
+		for(int i = 0; i < nums.length; i += 2){
+		             System.out.println(nums[i]);
+		}
+		Robot[] bot = new Robot[5];
+		int r = bot.length-1;
+		
+		
+		int x = 52;
+		int y = 25;
+
+		//your code goes here
+		int b = x;
+		x = y;
+		y = b;
+		System.out.println(x);  // should print 25  
+		System.out.println(y);  // should print 52
+		
+		
+		int[] num = {8,9,10};
+		for(int i = 0; i < num.length; i ++) {
+		System.out.println(num[i]);
+		}
+		
+		String[] wordList = {"izz", "molls", "ant", "bee"};
+		int h = wordList.length-1;
+		String g = wordList[0];
+		wordList[0] = wordList[h];
+		wordList[h] = g;
+		System.out.println(wordList[0] + "   " + wordList[h]);
+		
+	}
+
+*/
+	/*public class Drum{
+        public void beat()  { 
+        	Drum[] d = new Drum[5];
+        	for(int i = 0; i < d.length; i ++) {
+        		d[i] = new Drum();
+        		}
+        }*/
+}
+
